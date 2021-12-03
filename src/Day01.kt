c6d6a4eab7e208daec7ceead67610330eda28bfc
Tristan Rothman <tristan@labs.epiuse.com>
2021-12-03 12:22:58 +0200

    Solve Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,13 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
+    fun part1(input: List<Int>): Int {
+        return input.windowed(2).count { (x, y) -> x < y }
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
+    fun part2(input: List<Int>): Int {
+        return input.windowed(3).windowed(2).count { (x, y) -> x.sum() < y.sum() }
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
-
-    val input = readInput("Day01")
+    val input = readInputAsInts("Day01")
     println(part1(input))
     println(part2(input))
 }
